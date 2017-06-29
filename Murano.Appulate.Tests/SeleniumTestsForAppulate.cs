b40841f206d566ffc161d76d51b9f5ea695fce13
evreich <vlasov_godlike312@mail.ru>
2017-06-29 13:01:23 -0900

    Add project files.

---
+++ Murano.Appulate.Tests/SeleniumTestsForAppulate.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Murano.Appulate.Tests
+{
+    [TestClass]
+    public class SeleniumTestsForAppulate
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
